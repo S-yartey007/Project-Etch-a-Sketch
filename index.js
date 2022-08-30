ce7a192760189b3340e78cc49600f902da51766c
Emmanuel Yartey <supremeconquerorx@gmail.com>
2022-08-30 23:30:17 +0000

    using different implementation method to solve update problem

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
+let number;
 const container = document.querySelector('#container');
 const button = document.querySelector('.button');
-let number;
-let divs = [];
 
-function colorUp() {
-    this.style.backgroundColor = 'white';
-}
-function coloroff() {
+
+function changeColor() {
     this.style.backgroundColor = 'black';
 }
+function revertColor() {
+    this.style.backgroundColor = 'white';
+}
+
 function gridNumber() {
-     number = parseInt(prompt('Enter the number of grids'),10);
-     for(let i = 0 ; i < number; i++) {
-        const div = document.createElement('div');
-      divs.push(div) ;
-        
-    }
-    divs.forEach( div => {
-        div.classList.add('div');
-        container.appendChild(div);
-        div.addEventListener('mouseover', colorUp);
-        div.addEventListener('mouseout', coloroff);
+    let rows = [];
+    while(rows.length > 0) {
+        rows.pop()
+    };
+    console.log("prime array",rows)
+     number = parseInt(prompt('enter number of grid'),10);
+    for(let i = 0; i < number; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row')
+        rows.push(row)
+      }
+
+      rows.forEach( row => {
+       
+        for(let i = 0; i < number; i++) {
+            
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.addEventListener('mouseover', changeColor);
+            column.addEventListener('mouseout',revertColor);
+            row.appendChild(column);
+            
+        }
+        container.appendChild(row);
+        console.log("updated array", rows);
+      
     })
-   
 }
 
 
